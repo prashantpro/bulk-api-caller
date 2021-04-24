fa23a7097f22d8fb7bc78ad43a74d2a931020600
Prashant Padmanabhan <prashant.pro@gmail.com>
2021-04-24 22:05:13 +0530

    Initial test

---
+++ src/test/java/in/prashantpro/bulkapicaller/control/StringResponseMapperTest.java
@@ -0,0 +1,9 @@
+package in.prashantpro.bulkapicaller.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StringResponseMapperTest {
+
+  @org.junit.jupiter.api.Test
+  void map() {
+  }
+}
